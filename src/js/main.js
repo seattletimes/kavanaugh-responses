05526744d7a6f6d681f17ce4db801f4cd1244d28
sfmina <fmina@seattletimes.com>
2018-10-05 19:33:25 -0700

    First

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+// require("./lib/social");
+// require("./lib/ads");
+// var track = require("./lib/tracking");
+
+require("component-responsive-frame/child");
+
+var $ = require("./lib/qsa");
+//var colors = require("./lib/colors");
+
+//var matrix = $.one(".matrix");
+//var key = $.one(".key");
+//var detail = $.one(".detail");
+
+
